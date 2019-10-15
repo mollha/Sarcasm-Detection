6183e794466156bca90f3896a5008efb42462b75
mollha <molly.hayward@durham.ac.uk>
2019-10-15 08:28:09 +0100

    Implemented some of the data cleaner and twitter crawler

--- Code/TwitterCrawler/TwitterCrawler.py
+++ Code/TwitterCrawler/TwitterCrawler.py
@@ -3,6 +3,7 @@ from Code.TwitterCrawler.config import consumer_key, consumer_secret, access_key
 api = Api(consumer_key, consumer_secret, access_token_key=access_key, access_token_secret=access_secret)
 
 
-statuses = api.GetUserTimeline(screen_name="realDonaldTrump",  count=5)
-print([s.text for s in statuses])
 
+def get_some_tweets():
+    statuses = api.GetUserTimeline(screen_name="realDonaldTrump", count=5)
+    return [s.text for s in statuses]
