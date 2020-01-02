117e35493607f676395fde60de766a358a2b841d
mollha <mollyxhayward@gmail.com>
2020-01-02 00:31:42 +0000

    save progress

--- Code/Dataset/NewReview/MLmodels.py
+++ Code/Dataset/NewReview/MLmodels.py
@@ -5,9 +5,10 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import f1_score
 from sklearn.model_selection import train_test_split
 
-def my_SVM(sarcastic_data: pd.Dataframe, regular_data: pd.DataFrame):
+def my_SVM(sarcastic_data: pd.DataFrame, regular_data: pd.DataFrame):
     # ------------------------------ SPLIT DATA INTO TRAIN AND TEST -----------------------------------
     # shuffles data and splits
+    model = SVC(gamma='auto', C=10, kernel='linear')
     regular_train, regular_test, x_train, x_test = train_test_split(regular_data, regular_data['label'], test_size=0.3)
     ironic_train, ironic_test, y_train, y_test = train_test_split(sarcastic_data, sarcastic_data['label'], test_size=0.3)
     model.fit(regular_train, ironic_train)
@@ -17,7 +18,6 @@ def my_SVM(sarcastic_data: pd.Dataframe, regular_data: pd.DataFrame):
 
 # MACHINE LEARNING MODELS
 
-model = SVC(gamma='auto', C=10, kernel='linear')
 
 # Create a dataFrame with feature names as columns
 #
--- Code/Dataset/NewReview/Main.py
+++ Code/Dataset/NewReview/Main.py
@@ -1,6 +1,8 @@
 import spacy
 from Code.Dataset.NewReview.glove_vectors import GloVeConfig
 import pandas as pd
+from sklearn.svm import SVC
+from sklearn.model_selection import train_test_split
 import re
 import time
 from Code.Dataset.NewReview.MLmodels import my_SVM
@@ -28,7 +30,8 @@ if __name__ == '__main__':
     s_data = pd.DataFrame()
     sarcastic_data = pd.read_csv("Ironic.csv", encoding="ISO-8859-1")
     sarcastic_data['title_and_review'] = sarcastic_data["title"] + '. ' + sarcastic_data["review"]
-
+    sarcastic_data['data'] = sarcastic_data['title_and_review'].apply(data_cleaning)
+    sarcastic_data['label'] = 1
     print('Starting Data Cleaning...')
     s_data['data'] = sarcastic_data['title_and_review'].apply(data_cleaning)
     s_data['label'] = 1
@@ -36,6 +39,8 @@ if __name__ == '__main__':
     r_data = pd.DataFrame()
     regular_data = pd.read_csv("Regular.csv", encoding="ISO-8859-1")
     regular_data['title_and_review'] = regular_data["title"] + '. ' + regular_data["review"]
+    regular_data['data'] = regular_data['title_and_review'].apply(data_cleaning)
+    regular_data['label'] = 0
     r_data['data'] = regular_data['title_and_review'].apply(data_cleaning)
     r_data['label'] = 0
     print('Finished Data Cleaning')
@@ -51,7 +56,18 @@ if __name__ == '__main__':
     print('Total time: ', time.time() - start)
 
     # try and use our SVM
-    my_SVM(sarcastic_data, regular_data)
+    def my_SVM(sarcastic_data: pd.DataFrame, regular_data: pd.DataFrame):
+        # ------------------------------ SPLIT DATA INTO TRAIN AND TEST -----------------------------------
+        # shuffles data and splits
+        model = SVC(gamma='auto', C=10, kernel='linear')
+        regular_train, regular_test, x_train, x_test = train_test_split(regular_data, regular_data['label'],
+                                                                        test_size=0.3)
+        ironic_train, ironic_test, y_train, y_test = train_test_split(sarcastic_data, sarcastic_data['label'],
+                                                                      test_size=0.3)
+        model.fit(regular_train, ironic_train)
+        a = model.score(regular_test, ironic_test)
+        print('score: ', a)
+    # my_SVM(sarcastic_data, regular_data)
 
 
 
