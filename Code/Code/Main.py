999038b866c74fa3807e9bb5bd2222f9635bf024
mollha <mollyxhayward@gmail.com>
2020-02-11 11:01:11 +0000

    new main

--- Code/Code/Main.py
+++ Code/Code/Main.py
@@ -3,63 +3,57 @@ import pandas as pd
 import time
 from Code.Code.LSTM import get_LSTM
 from Code.Code.MLmodels import *
+import ast
 from sklearn.model_selection import train_test_split
 from Code.Code.DataPreprocessing import data_cleaning
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.decomposition import LatentDirichletAllocation
 from Code.Code.glove_vectors import GloVeConfig
-from sklearn.svm import SVC
-from spacy.tokenizer import Tokenizer
 from sklearn.model_selection import cross_val_score
 import numpy as np
 
 nlp = spacy.load('en_core_web_md')
-gb_token = Tokenizer(nlp.vocab)
-
-
-def tokenize(x):
-    #return gb_token(x)
-    return x.split()
 
 if __name__ == '__main__':
     start = time.time()
-    # --------- READING DATA ----------
-    data = pd.read_csv("Datasets/Sarcasm_Amazon_Review_Corpus/Data.csv", encoding="ISO-8859-1")
 
-    print('Starting Data Cleaning...')
-    data['clean_data'] = data['text_data'].apply(data_cleaning)
-    print('Finished Data Cleaning.')
+    # -------------------------- READING AND CLEANING DATA ------------------------------
+    re_run_cleaning = False
+    # Data cleaning has already been applied
+    data = pd.read_csv("Datasets/Sarcasm_Amazon_Review_Corpus/processed_data/OriginalData.csv", encoding="ISO-8859-1")
 
-    print('Tokenizing...')
-    token_data = data['clean_data'].apply(lambda x: tokenize(x))  # tokenizing sentences
-    print('Finished Tokenizing.')
+    if re_run_cleaning:
+        print('Starting Data Cleaning...')
+        data['clean_data'] = data['text_data'].apply(data_cleaning)
+    else:
+        data['clean_data'] = pd.read_csv("Datasets/Sarcasm_Amazon_Review_Corpus/processed_data/CleanData.csv",
+                                         encoding="ISO-8859-1")
 
-    print('Vectorizing...')
-    glove_embeddings = GloVeConfig(token_data)
-    vector = glove_embeddings.get_vectorized_data()  # my glove embeddings
-    # vector = data['clean_data'].apply(lambda x: nlp(x).vector)   # spaCy glove embeddings
-    # TODO need to make this cope with the scenario that no words in a sentence belong to glove dictionary
-    print('Finished Vectorizing.')
+    # --------------------------- TOKENIZE AND VECTORIZE -------------------------------
+    re_run_token_vector = False
 
-    print('Total time: ', time.time() - start)
+    if re_run_token_vector:
+        print('Tokenizing...')
+        token_data = data['clean_data'].apply(lambda x: [token.text for token in nlp(x)])  # tokenizing sentences
+        print('Vectorizing...')
+        glove_embeddings = GloVeConfig(token_data)
+        vector = glove_embeddings.get_vectorized_data()  # my glove embeddings
+        # TODO need to make this cope with the scenario that no words in a sentence belong to glove dictionary
+        vector.to_csv(path_or_buf="Datasets/Sarcasm_Amazon_Review_Corpus/processed_data/Vectors/glove_vectors.csv",
+                      index=False, header=['vector'])
+    else:
+        vector = pd.read_csv("Datasets/Sarcasm_Amazon_Review_Corpus/processed_data/Vectors/glove_vectors.csv",
+                             encoding="ISO-8859-1")['vector']
+        vector = vector.apply(lambda x: ast.literal_eval(x))
+
+    # ---------------------------------------------------------------------------------------------------------------
+
+    print('Configuration time: ', time.time() - start)
 
     # try and use our SVM
     print('Training ML models')
     labels = data['sarcasm_label']
-    print(vector[14])
-    clf = get_LSTM()
-    # scores = cross_val_score(clf, data['vector'], labels, cv=5)
-    scores = cross_val_score(clf, vector.apply(pd.Series), labels, cv=5, scoring='f1_macro')
+    classifier = get_model(0)
+    scores = cross_val_score(classifier, vector.apply(pd.Series), labels, cv=5, scoring='f1_macro')
     five_fold_cross_validation = np.mean(scores)
     print('Score: ', five_fold_cross_validation)
-
-    #
-    # training_data, testing_data, training_labels, testing_labels = train_test_split(vector.apply(pd.Series), labels, test_size=0.3)
-    #
-    # # lda_with_svm((training_data, testing_data, training_labels, testing_labels))
-    #
-    #
-    # untrained_model = get_model(0)
-    # trained_model, accuracy, f1_score_val = train_and_evaluate(untrained_model, (training_data, testing_data, training_labels, testing_labels))
-    # print('Accuracy: ', accuracy)
-    # print('F1 score: ', f1_score_val)
