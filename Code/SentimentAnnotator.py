54c2d069e59070d525fcba0742d3000b6a5362b0
mollha <molly.hayward@durham.ac.uk>
2020-02-22 17:19:38 +0000

    wrote sentiment annotator function

--- Code/SentimentAnnotator.py
+++ Code/SentimentAnnotator.py
@@ -1,14 +1,32 @@
 from pycorenlp import StanfordCoreNLP
+import spacy
+
+nlp = spacy.load('en_core_web_md')
 
 nlp_wrapper = StanfordCoreNLP('http://localhost:9000')
-doc = "I like this chocolate. This chocolate is not good. The chocolate is delicious. Its a very tasty chocolate. This is so bad"
-annot_doc = nlp_wrapper.annotate(doc,
-    properties={
-       'annotators': 'sentiment',
-       'outputFormat': 'json',
-       'timeout': 1000,
-    })
 
-for sentence in annot_doc["sentences"]:
-    print ( " ".join([word["word"] for word in sentence["tokens"]]) + " => " \
-        + str(sentence["sentimentValue"]) + " = "+ sentence["sentiment"])
+
+def sentiment_annotator(sentence: str) -> list:
+    """
+    Given a string, return the breakdown of sentiment values in the sentence, as well as the overall sentiment
+    :param sentence: A string of tokens
+    :return: list of sentiment counts e.g. [0.1, 0.3, 0.2, 0.3, 0.1, 3]
+    counts[0:4] --> frequency of tokens with sentiment values 0 - 4
+    counts[5] --> sentiment value of sentence
+    """
+    tokens = [token.text for token in nlp(sentence)]
+    counts = [0] * 5
+
+    settings = {'annotators': 'sentiment',
+                'outputFormat': 'json',
+                'timeout': 1000,
+                }
+
+    for doc in tokens:
+        sentiment_val = int(nlp_wrapper.annotate(doc, properties=settings)["sentences"][0]["sentimentValue"])
+        counts[sentiment_val] += 1
+
+    counts = [count / len(tokens) for count in counts]
+    overall_sentiment = int(nlp_wrapper.annotate(sentence, properties=settings)["sentences"][0]["sentimentValue"])
+    counts.append(overall_sentiment)
+    return counts
