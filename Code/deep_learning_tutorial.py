32a39dd7c6bbee08454b3bd61f0582c250f80d35
mollha <mollyxhayward@gmail.com>
2020-03-10 11:57:47 +0000

    helpppp

--- Code/deep_learning_tutorial.py
+++ Code/deep_learning_tutorial.py
@@ -3,15 +3,19 @@ import tensorflow as tf
 import pandas as pd
 import tensorflow_hub as hub
 from Code.DataPreprocessing import *
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
 import os
 import re
 from sklearn.metrics import f1_score, accuracy_score
 from random import randint
 from keras import backend as K
-from keras.layers import Dense, Input
+from keras.layers import Dense, Input,  Embedding
 from keras.models import Model, load_model, Sequential
 from keras.engine import Layer
 from sklearn.model_selection import train_test_split
+from keras.initializers import Constant
+
 import numpy as np
 
 # Initialize session
@@ -62,6 +66,22 @@ def get_clean_data_col(data_frame: pd.DataFrame, path_to_dataset_root: str, re_c
 data['clean_data'] = get_clean_data_col(data, path_to_dataset_root, False)
 
 
+class MultihotEmbedding(Layer):
+
+    def __init__(self, vocab_size, **kwargs):
+        self.trainable = False
+        self.vocab_size = vocab_size
+        super(MultihotEmbedding, self).__init__(**kwargs)
+
+    def call(self, x, mask=None):
+        self.get_embeddings = K.one_hot(x, num_classes=self.vocab_size)
+        self.reduce_embeddings = K.sum(self.get_embeddings,axis = -2)
+        return self.reduce_embeddings
+
+    def compute_output_shape(self, input_shape):
+        return input_shape[0], self.vocab_size
+
+
 class ElmoEmbeddingLayer(Layer):
     def __init__(self, **kwargs):
         self.dimensions = 1024
@@ -69,6 +89,7 @@ class ElmoEmbeddingLayer(Layer):
         super(ElmoEmbeddingLayer, self).__init__(**kwargs)
 
     def build(self, input_shape):
+        # Create a trainable weight variable for this layer.
         self.elmo = hub.Module('https://tfhub.dev/google/elmo/2', trainable=self.trainable,
                                name="{}_module".format(self.name))
 
@@ -76,10 +97,13 @@ class ElmoEmbeddingLayer(Layer):
         super(ElmoEmbeddingLayer, self).build(input_shape)
 
     def call(self, x, mask=None):
+        # this is where layer's logic lives, the forward function
+        print(x.shape)
         result = self.elmo(K.squeeze(K.cast(x, tf.string), axis=1),
                            as_dict=True,
                            signature='default',
                            )['default']
+        print(result.shape)
         return result
 
     def compute_mask(self, inputs, mask=None):
@@ -88,9 +112,80 @@ class ElmoEmbeddingLayer(Layer):
     def compute_output_shape(self, input_shape):
         return (input_shape[0], self.dimensions)
 
+#
+#  GloveEmbeddingLayer(input_shape=(1,), input_length= 1000, output_dim=50, trainable=False)
+# embedding_layer = Embedding(num_words,
+#                             EMBEDDING_DIM,
+#                             embeddings_initializer=Constant(embedding_matrix),
+#                             input_length=MAX_SEQUENCE_LENGTH,
+#                             trainable=False)
+
+
+class GloveEmbeddingLayer(Embedding):
+    def __init__(self, tokeniser, max_num_words):
+        self.MAX_SEQUENCE_LENGTH = 1000
+        self.MAX_NUM_WORDS = max_num_words
+        self.output_dim = 50
+        self.embeddings_matrix = self.compute_embedding_matrix(self.MAX_NUM_WORDS, tokeniser)
+        print(self.embeddings_matrix)
+        super(GloveEmbeddingLayer, self).__init__(input_dim=min(self.MAX_NUM_WORDS, len(tokeniser.word_index) + 1),
+                                                  output_dim=50,
+                                                  embeddings_initializer=Constant(self.embeddings_matrix),
+                                                  input_shape=(self.MAX_SEQUENCE_LENGTH,),
+                                                  trainable=False
+                                                  )
+
+    def compute_embedding_matrix(self, vocab_size, tokeniser):
+        with open('Datasets/GLOVEDATA/glove.twitter.27B.50d.txt', "r", encoding="utf-8") as file:
+            embeddings_index = {line.split()[0]: list(map(float, line.split()[1:])) for line in file}
+            del embeddings_index['0.45973']  # for some reason, this entry has 49 dimensions instead of 50
+
+        embedding_matrix = np.zeros((vocab_size, self.output_dim))
+        for word, i in tokeniser.word_index.items():
+            embedding_vector = embeddings_index.get(word)
+            if embedding_vector is not None:
+                embedding_matrix[i] = embedding_vector
+        return embedding_matrix
+
+# class GloveEmbeddingLayer(Layer):
+#     def __init__(self, vocab_size, tokeniser):
+#         self.trainable = False
+#         self.dimensions = 50
+#         self.embedding_matrix = self.compute_embedding_matrix(vocab_size, tokeniser)
+#         super(GloveEmbeddingLayer, self).__init__(vocab_size=vocab_size)
+#
+#     def compute_embedding_matrix(self, vocab_size, tokeniser):
+#         with open('Datasets/GLOVEDATA/glove.twitter.27B.50d.txt', "r", encoding="utf-8") as file:
+#             embeddings_index = {line.split()[0]: list(map(float, line.split()[1:])) for line in file}
+#             del embeddings_index['0.45973']  # for some reason, this entry has 49 dimensions instead of 50
+#
+#         embedding_matrix = np.zeros((vocab_size, self.dimensions))
+#         for word, i in tokeniser.word_index.items():
+#             embedding_vector = embeddings_index.get(word)
+#             if embedding_vector is not None:
+#                 embedding_matrix[i] = embedding_vector
+#         return embedding_matrix
+#
+#     def build(self, input_shape):
+#         # Create a trainable weight variable for this layer.
+#         self.trainable_weights = Constant(self.embedding_matrix)
+#         super(GloveEmbeddingLayer, self).build(input_shape)
+
+max_w = 20000
+max_s_l = 1000
+tokenizer = Tokenizer(num_words=max_w)
+tokenizer.fit_on_texts(data['clean_data'])
+sequences = tokenizer.texts_to_sequences(data['clean_data'])
+
+
+data_1 = pad_sequences(sequences, maxlen=max_s_l)
+
 def build_model():
     model = Sequential()
-    model.add(ElmoEmbeddingLayer(input_shape=(1,), input_dtype="string"))
+    # model.add(ElmoEmbeddingLayer(input_shape=(1,), input_dtype="string"))
+    # model.add(GloveEmbeddingLayer(input_shape=(1,), input_length= 1000, output_dim=50, trainable=False))
+    #model.add(MultihotEmbedding(vocab_size=40000, input_shape=(1,)))
+    model.add(GloveEmbeddingLayer(tokenizer, max_w))
     model.add(Dense(256, activation='relu'))
     model.add(Dense(1, activation='sigmoid'))
     model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
@@ -114,7 +209,7 @@ model = build_model()
 model.fit(train_text,
           train_label,
           validation_data=(test_text, test_label),
-          epochs=8,
+          epochs=4,
           batch_size=32)
 # model.summary()
 model.save('ElmoModel.h5')
